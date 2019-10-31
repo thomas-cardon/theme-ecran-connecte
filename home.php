15976c50846b041861be1e4f5809cff39bd252b3
Nicolas-Rohrbach <nicolas.rohrbach@etu.univ-amu.fr>
2019-10-31 15:32:39 +0100

    Modif affichage tv

--- home.php
+++ home.php
@@ -2,9 +2,15 @@
         $current_user = wp_get_current_user(); ?>
 <?php //if (! wp_is_mobile() ) {
 //    get_sidebar('left');
-//} ?>
-        <main <?php if(in_array("technicien", $current_user->roles)){  ?> id="content"> <?php } else { ?> id="content-twocolumns">
-                <?php } ?>
+//}
+$id = "content-twocolumns";
+
+if(in_array("technicien", $current_user->roles)) {
+    $id = "content";
+} else if(in_array("technicien", $current_user->roles)){
+    $id = "content_tv";
+} ?>
+        <main id="<?php $id ?>">
             <section>
                 <?php if(defined('TV_PLUG_PATH')) {
                     displaySchedule();
