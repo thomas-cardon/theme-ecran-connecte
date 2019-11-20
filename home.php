dd37817a85380845576566afca5329e62854017a
Nicolas-Rohrbach <nicolas.rohrbach@etu.univ-amu.fr>
2019-11-20 09:21:47 +0100

    Modif pour gestion admin

--- home.php
+++ home.php
@@ -1,30 +1,42 @@
 <?php get_header();
 $current_user = wp_get_current_user(); ?>
-<?php //if (! wp_is_mobile() ) {
-//    get_sidebar('left');
-//}
-?>
-<main id="
 <?php
+
 $id = "content-twocolumns";
-if (in_array("technicien", $current_user->roles)) {
-    $id = "content";
-} else if (in_array("technicien", $current_user->roles)) {
-    $id = "content_tv";
+$model = new AdminModel();
+$results = $model->getModif('column');
+$col = $results[0]->content;
+if ($col == 'left' || $col == 'two') {
+    if (!wp_is_mobile()) {
+        get_sidebar('left');
+    }
 }
-echo $id ?>">
-    <section>
-        <?php if (defined('TV_PLUG_PATH')) {
-            displaySchedule();
-        }
-        ?>
-    </section>
-</main>
-<?php
-//        if (wp_is_mobile()) {
-//            get_sidebar('left');
-//        }
-get_sidebar(); ?>
-<?php include_once 'template-parts/footer/footer_front.php'; ?>
+
+$id = $col;
+
+if (in_array("technicien", $current_user->roles)) {
+    $id = "none";
+} else if (in_array("technicien", $current_user->roles)) {
+    $id = "-tv";
+}
+echo '<main id="content-' . $id . '">
+    <section>';
+if (defined('TV_PLUG_PATH')) {
+    displaySchedule();
+}
+echo '
+</section>
+</main>';
+if ($col == 'left' || $col == 'two') {
+    if (wp_is_mobile()) {
+        get_sidebar('left');
+    }
+}
+
+if($col == 'right' || $col == 'two') {
+    get_sidebar();
+}
+
+include_once 'template-parts/footer/footer_front.php'; ?>
 </body>
 </html>
--- page.php
+++ page.php
@@ -1,16 +1,53 @@
-<?php get_header(); ?>
-<main id="content">
-    <section>
-        <?php if (have_posts()) : ?>
-            <?php while (have_posts()) : the_post(); ?>
-                <article class="post" id="post-<?php the_ID(); ?>">
-                    <!--    <h2><a href="<?php //the_permalink(); ?>" title="<?php //the_title(); ?>"><?php //the_title(); ?></a></h2> -->
-                    <div class="post_content"><?php the_content(); ?></div>
-                </article>
-            <?php endwhile; ?>
-        <?php endif; ?>
-    </section>
-</main>
-<?php get_footer(); ?>
+<?php get_header();
+$current_user = wp_get_current_user(); ?>
+<?php
+
+$id = "none";
+
+if(is_front_page()) {
+    $model = new AdminModel();
+    $results = $model->getModif('column');
+    $col = $results[0]->content;
+    if ($col == 'left' || $col == 'two') {
+        if (!wp_is_mobile()) {
+            get_sidebar('left');
+        }
+    }
+    $id = $col;
+}
+
+if (in_array("technicien", $current_user->roles)) {
+    $id = "none";
+} else if (in_array("technicien", $current_user->roles)) {
+    $id = "-tv";
+}
+echo '<main id="content-' . $id . '">
+    <section>'; ?>
+<?php if (have_posts()) : ?>
+    <?php while (have_posts()) : the_post(); ?>
+        <article class="post" id="post-<?php the_ID(); ?>">
+            <!--    <h2><a href="<?php //the_permalink(); ?>" title="<?php //the_title(); ?>"><?php //the_title(); ?></a></h2> -->
+            <div class="post_content"><?php the_content(); ?></div>
+        </article>
+    <?php endwhile; ?>
+<?php endif;
+echo '
+</section>
+</main>';
+if ($col == 'left' || $col == 'two') {
+    if (wp_is_mobile()) {
+        get_sidebar('left');
+    }
+}
+
+if(is_front_page()) {
+    if ($col == 'right' || $col == 'two') {
+        get_sidebar();
+    }
+    include_once 'template-parts/footer/footer_front.php';
+} else {
+    get_footer();
+} ?>
+
 </body>
 </html>
