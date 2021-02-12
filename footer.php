cfc71159c4ebf100810635610f167a055de8d562
Nils Ponsard <nilsponsard@gmail.com>
2021-02-12 17:15:22 +0100

    refactor footer

--- footer.php
+++ footer.php
@@ -12,13 +12,15 @@ if (get_theme_mod('ecran_connecte_footer_weather', 'right') == 'left') {
 
 $current_user = wp_get_current_user();
 if((in_array('etudiant', $current_user->roles) || in_array('television', $current_user->roles)) && is_front_page()) :
-    if(in_array('etudiant', $current_user->roles)) {
-        $class = 'footer_alert col-md-7 order-md-2';
-    } else {
+    $class_footer = "";
+    if(in_array('television', $current_user->roles))  {
         $class = 'footer_alert_television col-md-7 order-md-2';
-    } ?>
+    }else {
+        $class = 'footer_alert col-md-7 order-md-2';
+        $class_footer= "footer_alert_etudiant";
+    }?>
 
-    <footer class="row">
+    <footer class="row <?=$class_footer?>">
         <?php  if (get_theme_mod('ecran_connecte_footer_alert', 'show') == 'show') { ?>
             <div class = "<?php echo $class; ?>">
             <?php the_widget('WidgetAlert'); ?>
