2e17cbcbd9a620a42bb0801722031d719895dd4b
Thomas Cardon <thomas.cardon@pm.me>
2021-10-17 12:35:52 +0200

    Vue tablette (WIP)

---
+++ sidebar-tablet.php
@@ -0,0 +1,52 @@
+<aside data-active="1" class="sidebar-tablet">
+      <a href="/dashboard">
+        <div class="brand">
+          <img width="40px" height="40px" src="https://via.placeholder.com/150" />
+          <h3>Mode Tablette</h3>
+        </div>
+      </a>
+      <div class="list list-dark">
+        <a href="/dashboard" class="item">
+          <div class="icon">
+            <HiViewGridAdd />
+          </div>
+          Tableau de bord
+        </a>
+
+        <a href="/schedule" class="item">
+          <div class="icon">
+            <HiCalendar />
+          </div>
+          Emploi du temps
+        </a>
+
+        <a href="/posts/list" class="item">
+          <div class="icon">
+            <HiAnnotation />
+          </div>
+          Informations
+        </a>
+
+        <hr />
+
+        <a href="/subjects/list" class="item">
+          <div class="icon">
+            <HiTable />
+          </div>
+          Matières
+        </a>
+        <a href="/users/list" class="item">
+          <div class="icon">
+            <HiUsers />
+          </div>
+          Étudiants
+        </a>
+
+        <a href="/grades/list" class="item">
+          <div class="icon">
+            <HiPencilAlt />
+          </div>
+          Notes
+        </a>
+      </div>
+    </aside>
