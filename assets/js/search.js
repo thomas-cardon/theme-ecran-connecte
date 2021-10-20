811bc44ff9fe607dfc3d34478484449813ed764a
Thomas Cardon <thomas.cardon@pm.me>
2021-10-20 13:21:13 +0200

    Refactoring search-bar: suppression logique pour respecter MVC

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,75 +1,58 @@
-/**
- * Liste des entrées préenregistrées
- *
- */
-const entries = [
-  {
-    value: "Accueil",
-    action: () => {}
-  },
-  {
-    value: "Ajouter une information",
-    action: () => {}
+/* Liste des entrées (suggestions) de la barre de recherche */
+const entries = [];
+
+/* UI barre de recherche */
+(function() {
+  let found = entries;
+
+  function update(value, search, noSuggestionsDisplay, list = document.querySelector('.searchbar-suggestions')) {
+    if (!search || !noSuggestionsDisplay) return;
+
+    found = entries.filter(x => x.value.toLowerCase().includes(value.toLowerCase()));
+
+    if (value === '') list.replaceChildren();
+    else {
+      list.replaceChildren(...found.map(x => {
+        let li = document.createElement('li');
+
+        li.onclick = x.action;
+        li.innerText = x.value;
+
+        return li;
+      }));
+    }
+
+    if (value !== '' && found.length === 0) {
+      document.querySelector('.input-container').style.borderBottomLeftRadius = '.5em';
+      document.querySelector('.input-container').style.borderBottomRightRadius = '.5em';
+
+      noSuggestionsDisplay.style.display = 'block';
+    }
+    else if (value !== '' && found.length > 0) {
+      document.querySelector('.input-container').style.borderBottomLeftRadius = '0';
+      document.querySelector('.input-container').style.borderBottomRightRadius = '0';
+
+      noSuggestionsDisplay.style.display = 'none';
+    }
+    else if (value === '') {
+      document.querySelector('.input-container').style.borderBottomLeftRadius = '.5em';
+      document.querySelector('.input-container').style.borderBottomRightRadius = '.5em';
+
+      noSuggestionsDisplay.style.display = 'none';
+    }
   }
-];
-
-let found = entries;
-
-function update(value, search, noSuggestionsDisplay, list = document.querySelector('.searchbar-suggestions')) {
-  if (!search || !noSuggestionsDisplay) return;
-
-  found = entries.filter(x => x.value.toLowerCase().includes(value.toLowerCase()));
-
-  if (value === '') list.replaceChildren();
-  else {
-    list.replaceChildren(...found.map(x => {
-      let li = document.createElement('li');
-
-      li.onclick = x.action;
-      li.innerText = x.value;
-
-      return li;
-    }));
-  }
-
-  if (value !== '' && found.length === 0) {
-    document.querySelector('.input-container').style.borderBottomLeftRadius = '.5em';
-    document.querySelector('.input-container').style.borderBottomRightRadius = '.5em';
-
-    noSuggestionsDisplay.style.display = 'block';
-  }
-  else if (value !== '' && found.length > 0) {
-    document.querySelector('.input-container').style.borderBottomLeftRadius = '0';
-    document.querySelector('.input-container').style.borderBottomRightRadius = '0';
-
-    noSuggestionsDisplay.style.display = 'none';
-  }
-  else if (value === '') {
-    document.querySelector('.input-container').style.borderBottomLeftRadius = '.5em';
-    document.querySelector('.input-container').style.borderBottomRightRadius = '.5em';
-
-    noSuggestionsDisplay.style.display = 'none';
-  }
-}
 
 
-/**
- * TODO: placer la partie REST API dans le plugin
- * Quand le DOM (https://fr.wikipedia.org/wiki/Document_Object_Model#Aspects_techniques) est chargé, on commence à écouter les changements de valeur de la barre de recherche
- */
-window.addEventListener('DOMContentLoaded', event => {
-  let search = document.getElementById('search'), noSuggestionsDisplay = document.querySelector('.no-suggestions');
+  /**
+   * TODO: placer la partie REST API dans le plugin
+   * Quand le DOM (https://fr.wikipedia.org/wiki/Document_Object_Model#Aspects_techniques) est chargé, on commence à écouter les changements de valeur de la barre de recherche
+   */
+  window.addEventListener('DOMContentLoaded', event => {
+    let search = document.getElementById('search'), noSuggestionsDisplay = document.querySelector('.no-suggestions');
 
-  search
-  .oninput = event => update(event.target.value, search, noSuggestionsDisplay);
+    search
+    .oninput = event => update(event.target.value, search, noSuggestionsDisplay);
 
-  search.onblur = search.onfocus = event => update(search.value, search, noSuggestionsDisplay);
-
-  fetch(host + 'wp-json/amu-ecran-connectee/v1/ade/')
-  .then(res => res.json())
-  .then(data => data.forEach(code => {
-    entries.push({ value: code.title, action: () => alert('Action OK') })
-  }))
-  .catch(console.error);
-
-});
+    search.onblur = search.onfocus = event => update(search.value, search, noSuggestionsDisplay);
+  });
+})();
