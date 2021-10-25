9371061d5386b9bc5d7421da627207b756860153
Manaud <manaud57@gmil.com>
2021-10-25 10:59:05 +0200

    Avancement de la méto #2

--- footer-tablet.php
+++ footer-tablet.php
@@ -4,6 +4,10 @@
 	<div class="wave -two"></div>
 	<div class="wave -three"></div>
 	<div class="weathercon"><i class="fas fa-sun" style='color: #36326;'></i></div>
+	<div class="weathercon2"><i class="fas fa-wind" style='color: red;'></i></div>
+	<div class="weathercon3"><i class="fas fa-smog" style='color: red;'></i></div>
+	<div class="weathercon4"><i class="fas fa-cloud-rain" style='color: red;'></i></div>
+	<div class="weathercon5"><i class="fas fa-moon" style='color: red;'></i></div>
 	<div class="info">
 		<h2 class="location">Aix en Provence</h2>
 		<p class="date1">Lundi | OCT 25 | 9:50</p>
