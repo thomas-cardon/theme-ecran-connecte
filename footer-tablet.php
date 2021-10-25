3d7cbf9a2b216254739fa6faec3b2838a808f53d
Manaud <manaud57@gmil.com>
2021-10-25 10:00:20 +0200

    Avancement de la météo

--- footer-tablet.php
+++ footer-tablet.php
@@ -1,14 +1,17 @@
 </section>
-<aside class="sidebar-right">
-	<div class="rol_2">
-  		<a href="#" class="test">
-    		test
-  		</a>
-  		<a href="#" class="test">
-  			rzert
-  		</a> 
+<div class="box">
+	<div class="wave -one"></div>
+	<div class="wave -two"></div>
+	<div class="wave -three"></div>
+	<div class="weathercon"><i class="fas fa-sun" style='color: #36326;'></i></div>
+	<div class="info">
+		<h2 class="location">Aix en Provence</h2>
+		<p class="date1">Lundi | OCT 25 | 9:50</p>
+		<h1 class="temp">8 &deg;C | 47 &deg;F</h1>
 	</div>
-</aside>
+</div>
+
+</div>
 <?php wp_footer(); ?>
 <style type="text/css">
 	html {margin-top: 0px !important;}
