fb588836dd9a22c4d4c75c7e21fa4b3e741b7508
Thomas Cardon <thomas.cardon@pm.me>
2021-11-04 00:09:28 +0100

    Page: refactoring

--- page.php
+++ page.php
@@ -1,22 +1,18 @@
 <?php get_header(); ?>
     <!-- MAIN -->
     <main>
-        <div style="background-color: var(--color-primary-900) !important;">
-            <section class="content-area content-thin">
-                <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+      <div class="container-fluid px-0" style="background-color: #F2EFF2 !important;">
+            <section>
+                <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
                     <article class="article-full">
-                        <header>
-                            <!-- <h2><?php the_title(); ?></h2> -->
-                        </header>
                         <?php the_content(); ?>
                     </article>
                 <?php endwhile; else : ?>
                     <article>
-                        <p>Sorry, no page was found!</p>
+                        <p>Désolé, aucune page trouvée!</p>
                     </article>
                 <?php endif; ?>
             </section>
-            <?php //get_sidebar(); ?>
         </div>
     </main>
 <?php get_footer(); ?>
