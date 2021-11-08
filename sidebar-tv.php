0768da5ea5f4bf631ab7ffbd20988d50b7985ad4
Thomas Cardon <thomas.cardon@pm.me>
2021-11-08 18:55:02 +0100

    Barre latérale TV agrandie

--- sidebar-tv.php
+++ sidebar-tv.php
@@ -1,4 +1,4 @@
-<div class="col-auto col-md-5 col-xl-3 px-0" style="background-color: var(--color-secondary);">
+<div class="col-auto col-md-7 col-xl-3 px-0" style="background-color: var(--color-secondary);">
     <div class="d-flex flex-column text-white vh-100">
       <?php if (! function_exists('dynamic_sidebar') || ! dynamic_sidebar('Colonne Droite')) :
           the_widget('WidgetInformation');
