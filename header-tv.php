b309e2bd153933a7136dc08895da6389d4c60c77
Thomas Cardon <thomas.cardon@pm.me>
2021-11-09 12:55:02 +0100

    Police d'écriture Lato

--- header-tv.php
+++ header-tv.php
@@ -7,29 +7,10 @@
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <title><?php bloginfo('name'); ?></title>
-      <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-      <![endif]-->
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+      <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;900&display=swap" rel="stylesheet">
       <?php wp_head(); ?>
   </head>
   <!-- BODY -->
   <body <?php body_class(); ?>>
-
-<!-- HEADER
-<header>
-    <!-- NAV --
-    <?php if(members_current_user_has_role("television")) : ?>
-      <header class="text-white" style="background-color: #1476c6;">
-          <!-- NAV --
-          <nav class="navbar navbar-expand-lg navbar-dark">
-            <div class="container-fluid">
-              <a class="navbar-brand" href="http://localhost/tv">
-                <img class="bi mx-2" src="http://localhost/tv/wp-content/themes/theme-ecran-connecte/assets/media/header.png" alt="Logo du site" width="40" height="40">
-                TV Connectées
-              </a>
-            </div>
-          </nav>
-      </header>
-    <?php endif; ?>
-</header>-->
