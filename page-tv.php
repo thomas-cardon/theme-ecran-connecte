6f8cdc25a97bcd0ef8aa52469f6fd774539cac7a
Thomas Cardon <thomas.cardon@pm.me>
2021-11-12 15:56:25 +0100

    TV: ajout alertes

--- page-tv.php
+++ page-tv.php
@@ -3,19 +3,9 @@
     <main>
       <div class="container-fluid px-0" style="background-color: #F2EFF2 !important;">
         <div class="row flex-nowrap">
-            <section class="col py-3">
-                <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-                    <article class="article-full">
-                        <header>
-                            <!-- <h2><?php the_title(); ?></h2> -->
-                        </header>
-                        <?php the_content(); ?>
-                    </article>
-                <?php endwhile; else : ?>
-                    <article>
-                        <p>Sorry, no page was found!</p>
-                    </article>
-                <?php endif; ?>
+            <section class="col px-5 py-3">
+              <?php the_content(); ?>
+              <?php the_widget('WidgetAlert'); ?>
             </section>
             <?php get_sidebar('tv'); ?>
         </div>
