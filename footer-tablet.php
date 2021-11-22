2c7fb88fb0be679f3019287e6d58a2d1f9eb9266
Thomas Cardon <thomas.cardon@pm.me>
2021-11-22 19:41:39 +0100

    (TV secrétaires) Suppression de la météo

--- footer-tablet.php
+++ footer-tablet.php
@@ -1,21 +1,4 @@
 </section>
-<div class="box">
-	<div class="wave -one"></div>
-	<div class="wave -two"></div>
-	<div class="wave -three"></div>
-	<div class="weathercon"><i class="fas fa-sun" style='color: #36326;'></i></div>
-	<div class="weathercon2"><i class="fas fa-wind" style='color: red;'></i></div>
-	<div class="weathercon3"><i class="fas fa-smog" style='color: red;'></i></div>
-	<div class="weathercon4"><i class="fas fa-cloud-rain" style='color: red;'></i></div>
-	<div class="weathercon5"><i class="fas fa-moon" style='color: red;'></i></div>
-	<div class="info">
-		<h2 class="location">Aix en Provence</h2>
-		<p class="date1">Lundi | OCT 25 | 9:50</p>
-		<h1 class="temp">8 &deg;C | 47 &deg;F</h1>
-	</div>
-</div>
-
-</div>
 <?php wp_footer(); ?>
 <style type="text/css">
 	html {margin-top: 0px !important;}
