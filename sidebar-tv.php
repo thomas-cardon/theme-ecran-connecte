d3f3516d1b9073fb0511ae40ce421bf08549c7aa
Thomas Cardon <thomas.cardon@pm.me>
2021-12-03 11:20:55 +0100

    Position sticky

--- sidebar-tv.php
+++ sidebar-tv.php
@@ -1,5 +1,5 @@
-<div class="col-auto col-md-7 col-xl-3 px-0" style="background-color: var(--color-secondary);">
-    <div class="d-flex flex-column text-white vh-100">
+<div class="col-auto px-0 col-md-7 col-xl-3 position-sticky" style="background-color: var(--color-secondary);">
+    <div class="text-white d-flex flex-column vh-100">
       <?php if (! function_exists('dynamic_sidebar') || ! dynamic_sidebar('Colonne Droite')) :
           the_widget('WidgetInformation');
           the_widget('WidgetWeather');
